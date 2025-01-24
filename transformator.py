f448d408dba328d8f3df20cce375a0a2c2a5a714
DrozdovDan <daniil.drozdovjr@gmail.com>
2025-01-24 19:15:52 +0300

    AlekSet astar

--- transformator.py
+++ transformator.py
@@ -1,19 +1,31 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import os
 
-data = np.load(f'./output_tensor.npy', mmap_mode='c')
+main_dir = 'AlekSet'
+folders = ['train', 'test', 'val']
+dataset_dir = f'TransPath_{main_dir}'
 
-maps = np.expand_dims(data[..., 0] == 0, 1)
-starts = np.expand_dims(data[..., 1] == 2, 1)
-goals = np.expand_dims(data[..., 1] == 8, 1)
-focal = np.zeros_like(maps).astype(bool)
-cf = np.zeros_like(maps).astype(bool)
-abs = np.zeros_like(maps).astype(bool)
+for folder in folders: 
+    print(f'Start transforming {folder}...')
 
-os.makedirs(f'./eval_data/eval', exist_ok=True)
-np.save(f'./eval_data/eval/maps.npy', maps)
-np.save(f'./eval_data/eval/starts.npy', starts)
-np.save(f'./eval_data/eval/goals.npy', goals)
-np.save(f'./eval_data/eval/focal.npy', focal)
-np.save(f'./eval_data/eval/cf.npy', cf)
-np.save(f'./eval_data/eval/abs.npy', abs)
+    data = np.load(f'./{main_dir}/{folder}/maps.npy', mmap_mode='c')
+    maps = np.expand_dims(data[..., 0] == 0, 1)
+    starts = np.expand_dims(data[..., 3] == 1, 1)
+    goals = np.expand_dims(data[..., 1] == 1, 1)
+    focal = np.zeros_like(maps).astype(bool)
+    cf = np.load(f'./{main_dir}/{folder}/cf.npy', mmap_mode='c')
+    cf = np.expand_dims(cf, 1)
+    abs = np.expand_dims(data[..., 2], 1)
+
+    print(f'Start saving {folder}...')
+    os.makedirs(f'./{dataset_dir}/{folder}', exist_ok=True)
+
+    np.save(f'./{dataset_dir}/{folder}/maps.npy', maps)
+    np.save(f'./{dataset_dir}/{folder}/starts.npy', starts)
+    np.save(f'./{dataset_dir}/{folder}/goals.npy', goals)
+    np.save(f'./{dataset_dir}/{folder}/focal.npy', focal)
+    np.save(f'./{dataset_dir}/{folder}/cf.npy', cf)
+    np.save(f'./{dataset_dir}/{folder}/abs.npy', abs)
+
+    print(f'Finish saving {folder}!')
